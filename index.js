9a3424decffdcd2089ca3c062bd71bf3d3ea184b
zaibali <zaibali51@live.com>
2025-01-01 23:00:29 +0500

    add middleware for logging requests and refactor server port configuration

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from "express";
 
 const app = express();
+const port = 3001;
+
+app.use((req, res, next) => {
+  console.log("Inside Middleware");
+  next();
+})
 
 app.get("/", (req, res) => {
   console.log("request received", req.url);
   res.send("Hello World");
 });
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 app.get("/users", (req, res) => {
